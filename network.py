b9aaecdbacee63fc2df86c2f72696566aa4cc2d2
baovkb <21521860@gm.uit.edu.vn>
2023-10-29 18:41:37 +0700

    gs

--- network.py
+++ network.py
@@ -11,9 +11,7 @@ class Cellular():
         if res.find("There is no Mobile Broadband interface") > -1:
             print("Cellular is not available")
             raise ValueError
-        shell.terminate()
-        shell = subprocess.Popen('netsh mbn show profiles', shell=True, stdout=subprocess.PIPE)
-        res = str(shell.stdout.read())
+
         st = res.find("Cellular")
         name_cellular = ''
         while res[st] != ':':
@@ -23,7 +21,7 @@ class Cellular():
         self.name_cellular = name_cellular
         self.name_profile = '{2D591E2D-3C44-43BB-AF41-2FAB16FADA9B}'
 
-    def isConnect():
+    def isConnect(self):
         conn = httplib.HTTPSConnection("8.8.8.8", timeout=1.5)
         try:
             conn.request("HEAD", "/")
@@ -35,12 +33,14 @@ class Cellular():
         
     def connect(self):
         shell = subprocess.Popen('netsh mbn connect interface="%s" connmode=name name=%s' %(self.name_cellular,self.name_profile), shell=True, stdout=subprocess.PIPE)
+        shell.wait()
         shell.terminate()
 
     def disconnect(self):
         shell = subprocess.Popen('netsh mbn disconnect interface="%s"' %self.name_cellular, shell=True, stdout=subprocess.PIPE)
+        shell.wait()
         shell.terminate()
-        
+
 def isConnect():
     conn = httplib.HTTPSConnection("8.8.8.8", timeout=1.5)
     try:
